fda083fdb06cafe7cbe7edfbe283b9c2f321d020
Robson Gian Perassoli <robsonperassoli@gmail.com>
2016-10-27 21:54:25 -0200

    Add unit tests

--- src/main/java/br/com/robsonperassoli/todo/domain/TodoListRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.robsonperassoli.todo.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface TodoListRepository extends CrudRepository<TodoList, Long> {
-
-
-}
--- src/main/webapp/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('todoApp', [
-  'ngRoute',
-  'todoApp.todo_list'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/todo-list'});
-}]);
