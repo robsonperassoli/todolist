9819ef42814299315e22646eaa3b63fd232421b1
Robson Gian Perassoli <robsonperassoli@gmail.com>
2016-10-28 00:45:36 -0200

    Refactoring of the funcional test runner

--- app/src/test/java/br/com/robsonperassoli/todo/functional/FunctionalTests.java
+++ app/src/test/java/br/com/robsonperassoli/todo/functional/FunctionalTests.java
@@ -10,7 +10,6 @@ import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FunctionalTests {
 
--- app/src/test/java/br/com/robsonperassoli/todo/functional/IndexPageTest.java
+++ app/src/test/java/br/com/robsonperassoli/todo/functional/IndexPageTest.java
@@ -1,13 +1,16 @@
 package br.com.robsonperassoli.todo.functional;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(SpringRunner.class)
 public class IndexPageTest extends FunctionalTests {
 
     @Test
