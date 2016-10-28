48a08e32a5b99e61f912660005debd710ef7387b
Robson Gian Perassoli <robsonperassoli@gmail.com>
2016-10-28 00:42:00 -0200

    Refactoring of the funcional test runner

---
+++ app/src/test/java/br/com/robsonperassoli/todo/functional/FunctionalTests.java
@@ -0,0 +1,46 @@
+package br.com.robsonperassoli.todo.functional;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class FunctionalTests {
+
+    @LocalServerPort
+    private Integer serverPort;
+
+    private WebDriver driver;
+
+    @Before
+    public void openBrowser() {
+        if (System.getProperty("phantomjs.binary.path") != null) {
+            DesiredCapabilities dc = new DesiredCapabilities();
+            dc.setJavascriptEnabled(true);
+            driver = new PhantomJSDriver(dc);
+        } else {
+            throw new RuntimeException("Invalid PhantomJS path.");
+        }
+    }
+
+    @After
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public String getRootUrl() {
+        return "http://localhost:".concat(serverPort.toString());
+    }
+
+}
--- app/src/test/java/br/com/robsonperassoli/todo/functional/IndexPageTest.java
+++ app/src/test/java/br/com/robsonperassoli/todo/functional/IndexPageTest.java
@@ -1,51 +1,21 @@
 package br.com.robsonperassoli.todo.functional;
 
-import org.junit.After;
-import static org.junit.Assert.*;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.springframework.boot.context.embedded.LocalServerPort;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class IndexPageTest {
+import static org.junit.Assert.assertEquals;
 
-    @LocalServerPort
-    private Integer serverPort;
-
-    private WebDriver driver;
-
-    @Before
-    public void openBrowser() {
-        if (System.getProperty("phantomjs.binary.path") != null) {
-            DesiredCapabilities dc = new DesiredCapabilities();
-            dc.setJavascriptEnabled(true);
-            driver = new PhantomJSDriver(dc);
-        } else {
-            throw new RuntimeException("Invalid PhantomJS path.");
-        }
-    }
-
-    @After
-    public void closeBrowser() {
-        driver.quit();
-    }
+public class IndexPageTest extends FunctionalTests {
 
     @Test
     public void testTitleExists() throws InterruptedException {
-        driver.navigate().to("http://localhost:" + serverPort);
+        getDriver().navigate()
+                .to(getRootUrl());
 
-        List<WebElement> titleElements = driver.findElements(By.cssSelector("h3.text-muted"));
+        List<WebElement> titleElements = getDriver().findElements(By.cssSelector("h3.text-muted"));
 
         assertEquals("TODO", titleElements.get(0).getText());
     }
